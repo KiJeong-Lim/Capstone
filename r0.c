227930aa7d5b79886d38f9b272e44001b104e466
KiJeong-Lim <gijungdduk@naver.com>
2023-10-07 20:24:46 +0900

    initial

---
+++ r0.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+
+#define txMsg1 (txMsg[0])
+#define txMsg2 (txMsg[1])
+#define txMsg3 (txMsg[2])
+
+typedef struct TxMsg { int txMsg; double p; double v; double kp; double kd; double t_ff; } txMsg_t;
+
+FILE *file = NULL;
+txMsg_t txMsg[3] = { { .txMsg = 1 }, { .txMsg = 2 }, { .txMsg = 3 } };
+
+void pack_cmd(txMsg_t *msg, double p, double v, double kp, double kd, double t_ff)
+{
+    msg->p = p;
+    msg->v = v;
+    msg->kp = kp;
+    msg->kd = kd;
+    msg->t_ff = t_ff;
+}
+
+void serial_isr()
+{   
+    int x = 0, i = 0;
+
+    for (x = 0; x < 1000; x++) {
+        if (0<x && x<99) {
+            pack_cmd(&txMsg1, 0, 0, 0, 0, 0);
+            pack_cmd(&txMsg2, 0, 0, 0, 0, 0);
+            pack_cmd(&txMsg3, -0.20, 0, 4, 3, 0);
+        } else if(99<x && x<199) {
+            pack_cmd(&txMsg1, -0.1, 0, 18, 3.5, 0);
+            pack_cmd(&txMsg2, -0.115, 0, 18, 3.5, 0);
+            pack_cmd(&txMsg3, 0, 0, 15, 3, 0);
+        } else if(199<x && x<390) {
+            pack_cmd(&txMsg1, -0.11, 0, 10, 0.9, 0);
+            pack_cmd(&txMsg2, -0.1125, 0, 10, 1.5, -2);
+            pack_cmd(&txMsg3, -0.1, 0, 10, 1, 0);
+        } else if(389<x && x<400) {
+            pack_cmd(&txMsg2, -0.2925, 0, 20, 0.1, -2);
+        } else if(399<x && x<405) {
+            pack_cmd(&txMsg1, -0.1, 0, 10, 0.5, 0);
+            pack_cmd(&txMsg2, -0.18, 0, 16, 0, 2);
+            pack_cmd(&txMsg3, -0.175, 0, 15, 0.5, 0);
+        } else if(404<x && x<414){
+            pack_cmd(&txMsg1, -0.175, 0, 15, 0.5, 0);
+        } else if(413<x && x<415){
+            pack_cmd(&txMsg2, 0, 0.1, 15, 0, 2);
+        } else if(414<x && x<420){
+            pack_cmd(&txMsg1, -0.175, 0, 10, 1, 0);
+            pack_cmd(&txMsg3, 0, 0, 12, 0.3, 0);
+        } else if(419<x && x<424){
+            pack_cmd(&txMsg1, 0, 0, 10, 0.1, 0);
+        } else if(423<x && x<425){
+            pack_cmd(&txMsg2, -0.3, 0, 15, 0, -2);
+        } else if(424<x && x<430){
+            pack_cmd(&txMsg3, -0.225, 0, 10, 1, 0);
+        } else if(429<x && x<435){
+            pack_cmd(&txMsg1, -0.1125, 0, 10, 1, 0);
+            pack_cmd(&txMsg2, -0.15, 0, 10, 0, -2);
+        } else if(434<x && x<440){
+            pack_cmd(&txMsg2, -0.11, 0, 10, 2.7, -2);
+        } else if(439<x && x<450){
+            pack_cmd(&txMsg3, -0.0375, 0, 10, 1, 0);
+        } else if(600<x && x<700){
+            pack_cmd(&txMsg1, 0, 0, 4, 3.5, 0);
+            pack_cmd(&txMsg2, 0, 0, 2.5, 4.5, -4);
+            pack_cmd(&txMsg3, 0, 0, 10, 1.5, 0);
+        } else if(700<x && x<750){
+            pack_cmd(&txMsg1, 0, 0, 0, 0.1, 0);
+            pack_cmd(&txMsg2, 0, 0, 0, 0.1, 0);
+            pack_cmd(&txMsg3, 0, 0, 0, 0.1, 0);
+        }
+
+        for (i = 0; i < sizeof(txMsg) / sizeof(txMsg[0]); i++) {
+            fprintf(file, "%lf\t", txMsg[i].p);
+            fprintf(file, "%lf\t", txMsg[i].v);
+            fprintf(file, "%lf\t", txMsg[i].kp);
+            fprintf(file, "%lf\t", txMsg[i].kd);
+            fprintf(file, "%lf\t", txMsg[i].t_ff);
+        }
+        fprintf(file, " where x = %d\n", x);
+    }
+}
+
+int main(void)
+{
+    char file_name[32];
+
+    printf("I'm a program to generate a copy of the reference table.\n");
+    printf("Enter the name of the copy file: ");
+    fflush(stdout);
+    scanf("%s", file_name);
+    printf("[echo] %s\n", file_name);
+
+    file = fopen(file_name, "w+");
+
+    if(file != NULL)
+    {
+        serial_isr();
+        fclose(file);
+    }
+
+    file = NULL;
+    return 0;
+}
---
+++ tool.py
@@ -0,0 +1,250 @@
+# generate table
+
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+
+alpha = 0.03
+
+p1 = []
+v1 = []
+kp1 = []
+kd1 = []
+t1 = []
+
+p2 = []
+v2 = []
+kp2 = []
+kd2 = []
+t2 = []
+
+p3 = []
+v3 = []
+kp3 = []
+kd3 = []
+t3 = []
+
+dt = 0.01
+n = 0
+m = 0
+
+theta1 = []
+theta2 = []
+theta3 = []
+omega1 = []
+omega2 = []
+omega3 = []
+
+def load_cur(cur_file):
+    global n
+    global theta1
+    global theta2
+    global theta3
+    global omega1
+    global omega2
+    global omega3
+    n = 0
+    with open(cur_file, 'r') as data1_txt:
+        lns = data1_txt.readlines()
+        n = len(lns)
+        for i in range(n):
+            tmp = re.findall(r'-?\d+\.\d+', lns[i])
+            if i % 4 == 3:
+                pass
+            else:
+                theta = tmp[0]
+                omega = tmp[1]
+                if i % 4 == 0:
+                    theta1 = theta1 + [float(theta)]
+                    omega1 = omega1 + [float(omega)]
+                if i % 4 == 1:
+                    theta2 = theta2 + [float(theta)]
+                    omega2 = omega2 + [float(omega)]
+                if i % 4 == 2:
+                    theta3 = theta3 + [float(theta)]
+                    omega3 = omega3 + [float(omega)]
+
+def load_ref(ref_file):
+    global n
+    global m
+    m = 0
+    with open(ref_file, 'r') as refs_txt:
+        lns = refs_txt.readlines()
+        n = len(lns)
+        for i in range(n):
+            tmp = re.findall(r'-?\d+\.\d+', lns[i])
+            if not (tmp == []):
+                global p1
+                global v1
+                global kp1
+                global kd1
+                global t1
+                global p2
+                global v2
+                global kp2
+                global kd2
+                global t2
+                global p3
+                global v3
+                global kp3
+                global kd3
+                global t3
+                p1 = p1 + [float(tmp[0])]
+                v1 = v1 + [float(tmp[1])]
+                kp1 = kp1 + [float(tmp[2])]
+                kd1 = kd1 + [float(tmp[3])]
+                t1 = t1 + [float(tmp[4])]
+                p2 = p2 + [float(tmp[5])]
+                v2 = v2 + [float(tmp[6])]
+                kp2 = kp2 + [float(tmp[7])]
+                kd2 = kd2 + [float(tmp[8])]
+                t2 = t2 + [float(tmp[9])]
+                p3 = p3 + [float(tmp[10])]
+                v3 = v3 + [float(tmp[11])]
+                kp3 = kp3 + [float(tmp[12])]
+                kd3 = kd3 + [float(tmp[13])]
+                t3 = t3 + [float(tmp[14])]
+                m = m + 1
+
+def gen_new_tbl(tbl_file):
+    global p1
+    global p2
+    global p3
+    global v1
+    global v2
+    global v3
+    global kp1
+    global kp2
+    global kp3
+    global kd1
+    global kd2
+    global kd3
+    global t1
+    global t2
+    global t3
+
+    def upd(ref, cur):
+        return ref - alpha * (ref - cur)
+
+    with open(tbl_file, 'w+') as tbl_txt:
+        for i in range(m):
+            tbl_txt.write("{ ")
+            if 1 == 1:
+                tbl_txt.write("{")
+                tbl_txt.write(" .p_ref = %lf," % upd(p1[i], theta1[i]))
+                tbl_txt.write(" .v_ref = %lf," % upd(v1[i], omega1[i] * dt))
+                tbl_txt.write(" .kp_ref = %lf," % kp1[i])
+                tbl_txt.write(" .kd_ref = %lf," % kd1[i])
+                tbl_txt.write(" .t_ref = %lf," % t1[i])
+                tbl_txt.write(" }, ")
+            if 2 == 2:
+                tbl_txt.write("{ ")
+                tbl_txt.write(" .p_ref = %lf," % upd(p2[i], theta2[i]))
+                tbl_txt.write(" .v_ref = %lf," % upd(v2[i], omega2[i] * dt))
+                tbl_txt.write(" .kp_ref = %lf," % kp2[i])
+                tbl_txt.write(" .kd_ref = %lf," % kd2[i])
+                tbl_txt.write(" .t_ref = %lf," % t2[i])
+                tbl_txt.write(" }, ")
+            if 3 == 3:
+                tbl_txt.write("{ ")
+                tbl_txt.write(" .p_ref = %lf," % upd(p3[i], theta3[i]))
+                tbl_txt.write(" .v_ref = %lf," % upd(v3[i], omega3[i] * dt))
+                tbl_txt.write(" .kp_ref = %lf," % kp3[i])
+                tbl_txt.write(" .kd_ref = %lf," % kd3[i])
+                tbl_txt.write(" .t_ref = %lf," % t3[i])
+                tbl_txt.write(" }, ")
+            tbl_txt.write("},\n")
+        tbl_txt.flush()
+
+def plot_err(val_name):
+    global p1
+    global p2
+    global p3
+    global v1
+    global v2
+    global v3
+    global theta1
+    global theta2
+    global theta3
+    global omega1
+    global omega2
+    global omega3
+    global m
+
+    theta1_err = []
+    theta2_err = []
+    theta3_err = []
+    omega1_err = []
+    omega2_err = []
+    omega3_err = []
+
+    t = np.linspace(0, dt * n, m, endpoint=True)
+
+    for i in range(m):
+        theta1_err = theta1_err + [p1[i] - theta1[i]]
+        theta2_err = theta2_err + [p2[i] - theta2[i]]
+        theta3_err = theta3_err + [p3[i] - theta3[i]]
+        omega1_err = omega1_err + [v1[i] - dt * omega1[i]]
+        omega2_err = omega2_err + [v2[i] - dt * omega2[i]]
+        omega3_err = omega3_err + [v3[i] - dt * omega3[i]]
+
+    if val_name == 'theta1':
+        val = theta1_err
+    if val_name == 'theta2':
+        val = theta2_err
+    if val_name == 'theta3':
+        val = theta3_err
+    if val_name == 'omega1':
+        val = omega1_err
+    if val_name == 'omega2':
+        val = omega2_err
+    if val_name == 'omega3':
+        val = omega3_err
+
+    plt.plot(t,val,label=val_name+'_err')
+    plt.legend()
+    plt.show()
+
+if __name__ == '__main__':
+    b = True
+    for i in range(100):
+        if b:
+            cmd = input('cmd: ')
+            if cmd == "ref":
+                file = input('dir: ')
+                load_ref(file)
+            if cmd == "cur":
+                file = input('dir: ')
+                load_cur(file)
+            if cmd == "gen":
+                file = input('dir: ')
+                gen_new_tbl(file)
+            if cmd == "plot":
+                val_name = input('value: ')
+                plot_err(val_name)
+            if cmd == "clear":
+                n = 0
+                m = 0
+                theta1 = []
+                theta2 = []
+                theta3 = []
+                omega1 = []
+                omega2 = []
+                omega3 = []
+                p1 = []
+                v1 = []
+                kp1 = []
+                kd1 = []
+                t1 = []
+                p2 = []
+                v2 = []
+                kp2 = []
+                kd2 = []
+                t2 = []
+                p3 = []
+                v3 = []
+                kp3 = []
+                kd3 = []
+                t3 = []
+            if cmd == "quit":
+                b = False
